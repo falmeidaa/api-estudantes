c5d0a5a3ca29e4eaa4f860c9a57c49f7414541d7
Felipe Almeida <felipethiago27@gmail.com>
2021-11-25 21:26:03 -0300

    Instalando depencencias frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      '~': path.join(__dirname, 'tests')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }, {
+      test: /\.css$/i,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  },
+  devServer: {
+    contentBase: './dist',
+    writeToDisk: true,
+    historyApiFallback: true,
+    port: 8082
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      publicPath: './dist',
+      template: './public/template.html'
+
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+
+    }),
+    new Dotenv()
+  ]
+}
